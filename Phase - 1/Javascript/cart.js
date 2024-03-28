720ffaf51c6a729c3470e2e00460f11c4d3a819d
Hasin1040 <sheikhhasinishrak@gmail.com>
2024-03-28 13:19:17 +0300

    cart.js cont..

--- Phase - 1/Javascript/cart.js
+++ Phase - 1/Javascript/cart.js
@@ -3,21 +3,25 @@ let items = document.querySelectorAll(".add");
 let cars = [
     {
         name: "Ferrari",
+        id: "ferrari",
         price: 80000,
         added: 0
     },
     {
         name: "Lambo",
+        id: "lambo",
         price: 70000,
         added: 0
     },
     {
-        name: "Porche",
-        price: 50000,
+        name: "Porsche",
+        tag: "porsche",
+        price: 60000,
         added: 0
     },
     {
         name: "Toyota",
+        id: "toyota",
         price: 30000,
         added: 0
     },
@@ -25,7 +29,8 @@ let cars = [
 
 for (let i=0; i<items.length; i++){
     items[i].addEventListener("click", () =>{
-        itemsInCart();
+        itemsInCart(cars[i]);
+        invoice(cars[i]);
     })
 }
 
@@ -37,7 +42,8 @@ function loadCartDetails(){
 }
 
 
-function itemsInCart() {
+function itemsInCart(car) {
+  
     let quantity = localStorage.getItem("itemsInCart");
     quantity = parseInt(quantity);
     if (quantity) {
@@ -50,6 +56,45 @@ function itemsInCart() {
         document.querySelector(".cart-span span").textContent = 1;
     }
     
+    setItems(car);
 }
+
+function setItems(car){
+    let cartItems = localStorage.getItem("prodcutsInCart");
+    cartItems = JSON.parse(cartItems);
+    
+
+    if(cartItems!= null){
+        if(cartItems[car.id] == undefined){cartItems = {
+        ...cartItems,
+        [car.id]: car
+        }}
+        cartItems[car.id].added += 1;
+    }
+    else {
+        car.added = 1;
+        cartItems = {[car.id]: car}
+    }
+    
+    localStorage.setItem("prodcutsInCart", JSON.stringify(cartItems));
+}
+
+function invoice (car){
+    // console.log("the product price is", car.price);
+    let totCost = localStorage.getItem("invoice");
+    
+    console.log("My cart cost is ", totCost);
+    console.log(typeof totCost);
+
+    if(totCost != null){
+        totCost = parseInt(totCost);
+        localStorage.setItem("invoice", totCost + car.price);
+    }
+    else{
+        localStorage.setItem("invoice", car.price);
+    }
+    
+} 
+
 
 loadCartDetails();
